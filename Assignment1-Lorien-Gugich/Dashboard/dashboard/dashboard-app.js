289fe0c9e5f1f48815a43c92da1865d4cea47416
lorienellen <lorienellen@gmail.com>
2026-09-07 22:47:18 +1200

    Delete Assignment1-Lorien-Gugich directory

--- Assignment1-Lorien-Gugich/Dashboard/dashboard/dashboard-app.js
+++
@@ -1,9 +0,0 @@
-//IMPORT
-import { categoryChart } from "./Components/products-by-category.js";
-import { averageRatingChart } from "./Components/average-rating-per-product.js";
-import { stockLevelChart } from "./Components/inventory-stock-level.js";
-
-//APP CODES
-categoryChart.renderUI("category-chart");
-averageRatingChart.renderUI("rating-chart");
-stockLevelChart.renderUI("stock-chart");
